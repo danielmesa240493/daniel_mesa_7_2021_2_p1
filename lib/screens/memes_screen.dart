487b3915953bf76519f77e7a7eef91ae027a91af
danielmesa240493 <danielmesa240493@correo.itm.edu.co>
2021-10-23 17:10:01 -0500

    List Memes and ImageUrl

--- lib/screens/memes_screen.dart
+++ lib/screens/memes_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_memes_parcial/components/loader_component.dart';
 import 'package:flutter_memes_parcial/helpers/constans.dart';
@@ -30,7 +31,7 @@ class _MemesScreenState extends State<MemesScreen> {
         title: Text('Memes'),
       ),
       body: Center(
-        child: _showLoader ? LoaderComponent(text: 'Por favor espere...') : Text('Memes'),
+        child: _showLoader ? LoaderComponent(text: 'Por favor espere...') : _getContent(),
       ),
     );
   }
@@ -53,8 +54,68 @@ class _MemesScreenState extends State<MemesScreen> {
       _showLoader = false;
     });
 
-    print(response.body);
+    var body = response.body;
+    var decodedJson = jsonDecode(body);
+    if(decodedJson != null){
+      for (var item in decodedJson['data']) {
+        _memes.add(Meme.fromJson(item));
+      }
+    }
+
+    print(_memes);
   }
 
-  
+  Widget _getContent() {
+    return _memes.length == 0
+     ? _noContent()
+     : _getListView();
+  }
+
+  Widget _noContent() {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.all(20),
+        child: Text(
+          'No hay Memes.',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getListView() {
+    return ListView(
+      children: _memes.map((e) {
+        return Card(
+          child: InkWell(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Text(
+                    e.submissionTitle,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5,),
+                  Text(
+                    e.submissionUrl,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
 }
