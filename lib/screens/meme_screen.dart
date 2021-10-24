88dd917cb58356ef01ac42ccdbc964921c0852ec
danielmesa240493 <danielmesa240493@correo.itm.edu.co>
2021-10-23 22:14:58 -0500

    showMemeInfo

--- lib/screens/meme_screen.dart
+++ lib/screens/meme_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_memes_parcial/components/loader_component.dart';
 import 'package:flutter_memes_parcial/models/meme.dart';
 
 class MemeScreen extends StatefulWidget {
@@ -11,6 +13,8 @@ class MemeScreen extends StatefulWidget {
 }
 
 class _MemeScreenState extends State<MemeScreen> {
+  bool _showLoader = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +23,77 @@ class _MemeScreenState extends State<MemeScreen> {
           widget.meme.submissionTitle
         ),
       ),
-      body: Center(
-        child: Text(
-          widget.meme.submissionTitle
-        )
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              _showMemeInfo(),
+            ],
+          ),
+          _showLoader ? LoaderComponent(text: 'Cargando...',) : Container(),
+        ],
+      ),
+    );
+  }
+
+  Widget _showMemeInfo() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(5),
+      child: Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Text('URL Image: ', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              widget.meme.submissionUrl,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 5,),
+            Text('Autor: ', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              widget.meme.author,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 5,),
+            Text('Fecha de Creación: ', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              widget.meme.created,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 5,),
+            Text('Link: ', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              widget.meme.permalink,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 5,),
+            Text('ID: ', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              widget.meme.submissionId,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 5,),
+            Text('Marca de Tiempo: ', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              widget.meme.timestamp,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
